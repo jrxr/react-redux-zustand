0d0c461c379e88f39daec91c2a45a39654a1952c
agacy junior <juniormartinssom@gmail.com>
2023-06-29 11:41:17 -0300

    Loading course data

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -22,7 +22,7 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
   });
 
   const lessons = useAppSelector((state) => {
-    return state.player.course.modules[moduleIndex].lessons;
+    return state.player.course?.modules[moduleIndex].lessons;
   });
 
   return (
@@ -42,20 +42,21 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson, lessonIndex) => {
-            const isCurrent =
-              currentModuleIndex === moduleIndex &&
-              currentLessonIndex === lessonIndex;
-            return (
-              <Lesson
-                key={lesson.id}
-                title={lesson.title}
-                duration={lesson.duration}
-                isCurrent={isCurrent}
-                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
-              />
-            );
-          })}
+          {lessons &&
+            lessons.map((lesson, lessonIndex) => {
+              const isCurrent =
+                currentModuleIndex === moduleIndex &&
+                currentLessonIndex === lessonIndex;
+              return (
+                <Lesson
+                  key={lesson.id}
+                  title={lesson.title}
+                  duration={lesson.duration}
+                  isCurrent={isCurrent}
+                  onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+                />
+              );
+            })}
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -4,13 +4,14 @@ import { next, useCurrentLesson } from "../store/slices/player";
 
 export function Video() {
   const dispatch = useDispatch()
-
   const { currentLesson } = useCurrentLesson()
 
   function handlePlayNext() {
     dispatch(next())
   }
 
+  if (!currentLesson) return null;
+  
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
